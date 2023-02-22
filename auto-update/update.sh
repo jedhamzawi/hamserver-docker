e6fccd821655df71710b5fb1055848a661999c70
Jake Hamzawi <jedhamzawi@protonmail.com>
2023-02-21 20:02:16 -0700

    Replace pihole with adguard home

--- auto-update/update.sh
+++ auto-update/update.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 ROOT=/home/hamsesh/docker/auto-update
 
 for d in $ROOT/*/ ; do
     test "$d" = $ROOT/manual-update/ && continue
     cd "$d"
-    docker compose down
-    docker compose pull
-    docker compose up -d
+    docker compose pull && docker compose up -d
 done
 docker system prune -af
 echo "Update completed! $(date)" >> /var/log/docker/update.log
